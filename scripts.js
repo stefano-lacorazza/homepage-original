0875a35e6828eab4a6daf38002bb7ace09ef9992
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2024-06-01 22:45:28 -0500

    Added sounds

--- scripts.js
+++ scripts.js
@@ -36,12 +36,19 @@ window.addEventListener('keydown', function(e) {
     player.classList.add('caminar')
     player.classList.remove('mirror')
     updateElement(player, +speed, 0)
+    audio.volume = 0.2;
+    audio.play();
+    document.getElementById('button').style.display = 'block';
+    
   }
   // Cursor hacia la izquierda
   else if (e.keyCode == 37) {
     player.classList.add('caminar')
     player.classList.add('mirror')
     updateElement(player, -speed, 0)
+    audio.volume = 0.2;
+    audio.play();
+    document.getElementById('button').style.display = 'block';
   }
   
 
@@ -56,3 +63,30 @@ window.addEventListener('keyup', function(e) {
 
 
 updateElement(player, 0, 0)
+
+const button = document.querySelector("#button");
+const icon = document.querySelector("#button > i");
+const audio = document.querySelector("audio");
+
+button.addEventListener("click", () => {
+  if (audio.paused) {
+    audio.volume = 0.2;
+    audio.play();
+    icon.classList.remove('fa-volume-up');
+    icon.classList.add('fa-volume-mute');
+    
+  } else {
+    audio.pause();
+    icon.classList.remove('fa-volume-mute');
+    icon.classList.add('fa-volume-up');
+  }
+  button.classList.add("fade");
+});
+
+var avatar = document.getElementById('profile-pic');
+var hoverSound = document.getElementById('hoverSound');
+
+avatar.addEventListener('mouseover', function() {
+  hoverSound.play();
+});
+
